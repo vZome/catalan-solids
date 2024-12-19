73b61eddb646e84bac7d68217af91422647666a3
undeterminstic <73261021+PlayfulMathematician@users.noreply.github.com>
2024-12-19 12:41:34 -0700

    WHAT!!!!

--- archimedean-solids-models.js
+++ archimedean-solids-models.js
@@ -1,5 +1,5 @@
 export const models = [
   { id:1 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated tetrahedron", url:"uploads/ThynStyx/Golden-Truncated-Tetrahedron.vZome"},
   { id:2 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Cuboctahedron", url:"uploads/ThynStyx/Golden-Cuboctahedron.vZome"},
-  { id:3 , field:"Root2", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Truncated cube", url:"uploads/ThynStyx/√2-field-Truncated-Cube.vZome"},
+  { id:3 , field:"Root2", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated cube", url:"uploads/ThynStyx/√2-field-Truncated-Cube.vZome"},
   ]
