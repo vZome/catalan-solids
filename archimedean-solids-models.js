ed5a2e7b91c9fc7fa1c607175814bdd0dbe2793b
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2024-12-20 09:20:17 -0700

    added Truncated Octahedron

--- archimedean-solids-models.js
+++ archimedean-solids-models.js
@@ -2,7 +2,7 @@ export const models = [
   { id:1 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated tetrahedron", url:"uploads/ThynStyx/Golden-Truncated-Tetrahedron.vZome"},
   { id:2 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Cuboctahedron", url:"uploads/ThynStyx/Golden-Cuboctahedron.vZome"},
   { id:3 , field:"Root2", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Truncated cube", url:"uploads/ThynStyx/√2-field-Truncated-Cube.vZome"},
-  { id:4 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated octahedron", url:""},
+  { id:4 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated octahedron", url:"uploads/undeterministic/Truncated-Octahedron.vZome"},
   { id:5 , field:"Root2", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Rhombicuboctahedron", url:""},
   { id:6 , field:"Root2",edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Truncated cuboctahedron", url:""},
   { id:7 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Icosidodecahedron", url:""},
