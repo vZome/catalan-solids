9a7b92ecc7deb7a21fe6be062838da1c022ccadc
John Hall <78449548+ThynStyx@users.noreply.github.com>
2024-12-30 19:23:42 +0000

    Update archimedean-solids-models.js
    A7 added

--- archimedean-solids-models.js
+++ archimedean-solids-models.js
@@ -5,7 +5,7 @@ export const models = [
   { id:4 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated octahedron", url:"uploads/undeterministic/Truncated-Octahedron.vZome"},
   { id:5 , field:"Root2", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Rhombicuboctahedron", url:""},
   { id:6 , field:"Root2",edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Truncated cuboctahedron", url:""},
-  { id:7 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Icosidodecahedron", url:""},
+  { id:7 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Icosidodecahedron", url:"uploads/ThynStyx/Golden-Icosidodecahedron.vZome"},
   { id:8 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated dodecahedron", url:"uploads/ThynStyx/Golden-Truncated-Dodecahedron.vZome"},
   { id:9 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated icosahedron", url:""},
   { id:10 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Rhombicosidodecahedron", url:""},
