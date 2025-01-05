8a9845eb6d5bd75595853fe28fda74a7778d192b
John Hall <78449548+ThynStyx@users.noreply.github.com>
2025-01-05 22:18:38 +0000

    Update archimedean-solids-models.js
    snub dedec dextro mod

--- archimedean-solids-models.js
+++ archimedean-solids-models.js
@@ -11,7 +11,7 @@ export const models = [
   { id:10 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Rhombicosidodecahedron", url:"uploads/ThynStyx/Golden-Rhombicosidodecahedron-SV.vZome"},
   { id:11 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Truncated icosidodecahedron", url:"uploads/ThynStyx/Golden-Truncated-Icosidodecahedron-ONLINE.vZome"},
   { id:12 , field:"snubCube", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Dextro snub cube", url:"uploads/ThynStyx/Snub-field-Dextro-Snub-cube-ONLINE.vZome"},
-  { id:13 , field:"snubDodec", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Dextro snub dodecahedron", url:"uploads/ThynStyx/Snub-dodec-dextro-jk.vZome"},
+  { id:13 , field:"snubDodec", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Dextro snub dodecahedron", url:"uploads/ThynStyx/Snub-dodec-dextro-jk-ONLINE.vZome"},
   { id:14 , field:"snubCube", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Laevo snub cube", url:"uploads/ThynStyx/Snub-field-Laevo-Snub-cube-ONLINE.vZome"},
-  { id:15 , field:"snubDodec", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Laevo snub dodecahedron", url:"uploads/ThynStyx/snub-dodec-laevo-jk.vZome"},
+  { id:15 , field:"snubDodec", edgescene:"Edges", facescene:"Faces", zometool:"false", title:"Laevo snub dodecahedron", url:"uploads/ThynStyx/snub-dodec-laevo-jk-ONLINE.vZome"},
   ]
